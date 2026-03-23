b5a696fb9b1ef56589fef659033db96bdcfff16d
prathmeshpotdar <81902791+prathmeshpotdar@users.noreply.github.com>
2026-03-23 13:06:30 +0530

    Delete backend directory

--- backend/src/main/java/com/resume/analyzer/repository/AnalysisRepository.java
+++
@@ -1,8 +0,0 @@
-package com.resume.analyzer.repository;
-
-import com.resume.analyzer.entity.Analysis;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnalysisRepository extends JpaRepository<Analysis, Long> {
-    Analysis findByResumeId(Long resumeId);
-}
--- backend/src/main/java/com/resume/analyzer/repository/ResumeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.resume.analyzer.repository;
-
-import com.resume.analyzer.entity.Resume;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ResumeRepository extends JpaRepository<Resume, Long> {
-    List<Resume> findByUserId(Long userId);
-}
